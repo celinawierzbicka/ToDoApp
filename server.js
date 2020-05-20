32a8092d722bef08afa24e29175d685d897cdecd
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-05-20 10:24:37 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+
+mongoose.connect('mongodb://localhost:27017/mongoose_express_todos', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(function(){console.log('Database connected...')
+});
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mustacheExpressInstance = mustacheExpress();
+mustacheExpressInstance.cache = null;
+app.engine('mustache', mustacheExpressInstance);
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use('/', routes);
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+})
